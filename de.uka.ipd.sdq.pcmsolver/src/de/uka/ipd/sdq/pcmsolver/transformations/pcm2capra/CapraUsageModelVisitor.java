67a1aa9f3239cb3c3b06966928d7506caee88090
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2015-06-24 11:58:47 +0000

    Moved to clean project structure
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Analyzer/Solver/trunk@27672 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ de.uka.ipd.sdq.pcmsolver/src/de/uka/ipd/sdq/pcmsolver/transformations/pcm2capra/CapraUsageModelVisitor.java
@@ -0,0 +1,9 @@
+//package de.uka.ipd.sdq.pcmsolver.transformations.pcm2capra;
+//
+//import de.uka.ipd.sdq.capra.core.CapraExpression;
+//import org.palladiosimulator.pcm.usagemodel.util.UsagemodelSwitch;
+//
+//
+//public class CapraUsageModelVisitor extends UsagemodelSwitch<CapraExpression> {
+//
+//}
