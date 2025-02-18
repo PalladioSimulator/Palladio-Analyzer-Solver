ea99d3ef7c866f28dff862f95de57129a8e2b05d
Ralf Sieger <sieger@fzi.de>
2025-02-18 13:27:18 +0100

    removed concrete class from signature

--- bundles/org.palladiosimulator.solver/src/org/palladiosimulator/solver/runconfig/PCMSolverLaunchConfigurationDelegate.java
+++ bundles/org.palladiosimulator.solver/src/org/palladiosimulator/solver/runconfig/PCMSolverLaunchConfigurationDelegate.java
@@ -1,6 +1,7 @@
 package org.palladiosimulator.solver.runconfig;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.log4j.Level;
 import org.eclipse.core.runtime.CoreException;
@@ -62,8 +63,8 @@ public class PCMSolverLaunchConfigurationDelegate
     }
 
     @Override
-    protected ArrayList<LoggerAppenderStruct> setupLogging(Level logLevel) throws CoreException {
-        ArrayList<LoggerAppenderStruct> loggerList = super.setupLogging(logLevel);
+    protected List<LoggerAppenderStruct> setupLogging(Level logLevel) throws CoreException {
+        List<LoggerAppenderStruct> loggerList = new ArrayList<>(super.setupLogging(logLevel));
         loggerList.add(setupLogger("org.palladiosimulator.solver", logLevel,
                 Level.DEBUG == logLevel ? DETAILED_LOG_PATTERN : SHORT_LOG_PATTERN));
 
