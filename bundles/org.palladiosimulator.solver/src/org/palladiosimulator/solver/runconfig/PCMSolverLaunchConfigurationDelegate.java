f668ed6b5d3b887907668ea36ed07e0f41fdf585
Ralf Sieger <sieger@fzi.de>
2025-02-18 08:16:07 +0100

    added createExceptionHandler() implementation

--- bundles/org.palladiosimulator.solver/src/org/palladiosimulator/solver/runconfig/PCMSolverLaunchConfigurationDelegate.java
+++ bundles/org.palladiosimulator.solver/src/org/palladiosimulator/solver/runconfig/PCMSolverLaunchConfigurationDelegate.java
@@ -10,52 +10,52 @@ import org.palladiosimulator.analyzer.accuracy.jobs.AccuracyInfluenceAnalysisJob
 import org.palladiosimulator.analyzer.workflow.configurations.AbstractPCMLaunchConfigurationDelegate;
 import org.palladiosimulator.analyzer.workflow.core.configurations.PCMWorkflowConfigurationBuilder;
 
+import de.uka.ipd.sdq.workflow.WorkflowExceptionHandler;
 import de.uka.ipd.sdq.workflow.jobs.IJob;
 import de.uka.ipd.sdq.workflow.launchconfig.core.AbstractWorkflowConfigurationBuilder;
 import de.uka.ipd.sdq.workflow.logging.console.LoggerAppenderStruct;
+import de.uka.ipd.sdq.workflow.ui.UIBasedWorkflowExceptionHandler;
 
 public class PCMSolverLaunchConfigurationDelegate
-		extends
-		AbstractPCMLaunchConfigurationDelegate<PCMSolverWorkflowRunConfiguration> {
+        extends AbstractPCMLaunchConfigurationDelegate<PCMSolverWorkflowRunConfiguration> {
 
-	@Override
-	protected IJob createWorkflowJob(PCMSolverWorkflowRunConfiguration config,
-			ILaunch launch) throws CoreException {
+    @Override
+    protected WorkflowExceptionHandler createExceptionHandler(boolean interactive) {
+        return new UIBasedWorkflowExceptionHandler(!interactive);
+    }
 
-		// To enable accuracy analysis, the former top-level job (PCMSolverReliabilityJob)
-		// is replaced through a new AccuracyInfluenceAnalysisJob:
-		PCMSolverWorkflowJobBuilder jobBuilder = new PCMSolverWorkflowJobBuilder(launch);
-		return new AccuracyInfluenceAnalysisJob(config, jobBuilder);
-	}
+    @Override
+    protected IJob createWorkflowJob(PCMSolverWorkflowRunConfiguration config, ILaunch launch) throws CoreException {
 
-	@Override
-	protected PCMSolverWorkflowRunConfiguration deriveConfiguration(
-			ILaunchConfiguration configuration, String mode)
-			throws CoreException {
-		PCMSolverWorkflowRunConfiguration solverConfiguration = new PCMSolverWorkflowRunConfiguration();
+        // To enable accuracy analysis, the former top-level job (PCMSolverReliabilityJob)
+        // is replaced through a new AccuracyInfluenceAnalysisJob:
+        PCMSolverWorkflowJobBuilder jobBuilder = new PCMSolverWorkflowJobBuilder(launch);
+        return new AccuracyInfluenceAnalysisJob(config, jobBuilder);
+    }
 
-		AbstractWorkflowConfigurationBuilder builder;
+    @Override
+    protected PCMSolverWorkflowRunConfiguration deriveConfiguration(ILaunchConfiguration configuration, String mode)
+            throws CoreException {
+        PCMSolverWorkflowRunConfiguration solverConfiguration = new PCMSolverWorkflowRunConfiguration();
 
-		builder = new PCMWorkflowConfigurationBuilder(configuration, mode);
-		builder.fillConfiguration(solverConfiguration);
+        AbstractWorkflowConfigurationBuilder builder;
 
-		builder = new PCMSolverConfigurationBasedConfigBuilder(configuration,
-				mode);
-		builder.fillConfiguration(solverConfiguration);
+        builder = new PCMWorkflowConfigurationBuilder(configuration, mode);
+        builder.fillConfiguration(solverConfiguration);
 
-		return solverConfiguration;
-	}
+        builder = new PCMSolverConfigurationBasedConfigBuilder(configuration, mode);
+        builder.fillConfiguration(solverConfiguration);
 
-	@Override
-	protected ArrayList<LoggerAppenderStruct> setupLogging(Level logLevel)
-			throws CoreException {
-		ArrayList<LoggerAppenderStruct> loggerList = super
-				.setupLogging(logLevel);
-		loggerList.add(setupLogger("org.palladiosimulator.solver", logLevel,
-				Level.DEBUG == logLevel ? DETAILED_LOG_PATTERN
-						: SHORT_LOG_PATTERN));
+        return solverConfiguration;
+    }
 
-		return loggerList;
-	}
+    @Override
+    protected ArrayList<LoggerAppenderStruct> setupLogging(Level logLevel) throws CoreException {
+        ArrayList<LoggerAppenderStruct> loggerList = super.setupLogging(logLevel);
+        loggerList.add(setupLogger("org.palladiosimulator.solver", logLevel,
+                Level.DEBUG == logLevel ? DETAILED_LOG_PATTERN : SHORT_LOG_PATTERN));
+
+        return loggerList;
+    }
 
 }
