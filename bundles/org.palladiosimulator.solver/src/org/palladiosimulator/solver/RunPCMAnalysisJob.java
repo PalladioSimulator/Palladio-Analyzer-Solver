d0365f240c61fbf64a2a05f0cf713daa2789bd74
Ralf Sieger <sieger@fzi.de>
2025-02-19 08:59:12 +0100

    adapted to moved PCMResourceSetPartition

--- bundles/org.palladiosimulator.solver/src/org/palladiosimulator/solver/RunPCMAnalysisJob.java
+++ bundles/org.palladiosimulator.solver/src/org/palladiosimulator/solver/RunPCMAnalysisJob.java
@@ -2,7 +2,7 @@ package org.palladiosimulator.solver;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.palladiosimulator.analyzer.workflow.blackboard.PCMResourceSetPartition;
+import org.palladiosimulator.analyzer.workflow.core.blackboard.PCMResourceSetPartition;
 import org.palladiosimulator.analyzer.workflow.jobs.LoadPCMModelsIntoBlackboardJob;
 import org.palladiosimulator.solver.models.PCMInstance;
 import org.palladiosimulator.solver.runconfig.MessageStrings;
